2bd995c0e9cf5162bc30118681fbb7df711c52f6
bigdog0044 <jambo.peach@outlook.com>
2025-05-23 22:44:26 +0100

    added the part to the program in which is processes the user input and passes it through the AI model
    Improved the AI model accuracy

--- website files/webhosting.py
+++ website files/webhosting.py
@@ -2,10 +2,21 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import current_app
+import pickle
+import numpy as np
 #from validateData import validData
 app = Flask(__name__)
 
-def AIProcessing(ageInput,numVecInput,experienceInput,speedInput):
+def AIProcessing( age, totalVehicle, experience, alcoholConsumption, trafficDensity, weather, roadType,
+            timeOfDay, roadCondition, vehicleType, roadLightCondition, speedLimit, accidentSeverity):
+    model = pickle.load(open("model.pkl", "rb"))
+    data = np.array([weather, roadType, timeOfDay,trafficDensity,
+                speedLimit, totalVehicle, alcoholConsumption, accidentSeverity,
+                roadCondition, vehicleType, age, experience,
+                roadLightCondition])
+    print(data)
+    result = model.predict(data.reshape(-1,1))
+    print(result)
     return 0
 
 #@app.route("/sendInfo", methods=["POST", "GET"])
@@ -18,7 +29,8 @@ def validData(ageInput,numVecInput,experienceInput,speedInput):
             return "Error: inputs can't be negative"
     except ValueError:
         return "Error: incorrect format detected. Enter only numbers as the input values"
-    return None
+    
+    return None #None is used to indicate no error has happened
 
 @app.route("/", methods=["POST", "GET"])
 def mainPage(name=None):
@@ -32,8 +44,26 @@ def mainPage(name=None):
                 request.form.get('experience'), 
                     request.form.get('speed_limit'))
         if error == None:
+            age = request.form.get('driverAge')
+            totalVehicle = int(request.form.get('numVechicle'))
+            experience = int(request.form.get('experience'))
+            alcoholConsumption = int(request.form.get('alcohol-yes-or-no'))
+            trafficDensity = int(request.form.get('traffic-density'))
+            weather = int(request.form.get('weather'))
+            roadType = int(request.form.get('road_type'))
+            timeOfDay = int(request.form.get('time_of_day'))
+            roadCondition = int(request.form.get('road_condition'))
+            vehicleType = int(request.form.get('vehicle_type'))
+            roadLightCondition = int(request.form.get('road_light_condition'))
+            speedLimit = int(request.form.get('speed_limit'))
+            accidentSeverity = int(request.form.get('accident_severity'))
+            
+            
+            AIProcessing(age, totalVehicle, experience, alcoholConsumption, trafficDensity, weather,
+                         roadType, timeOfDay, roadCondition, vehicleType, roadLightCondition,
+                         speedLimit, accidentSeverity)
             #add stuff here that gets returned by the AI algorithm
-            return render_template("form.html")
+            return render_template("form.html", result = 1)
         else:
             return render_template("form.html", errorMSG=error)
         
